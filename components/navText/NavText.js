b73876f4c4d15c0d76a9def180b18cb486830be5
HassanThePassionate <maqsoodia777@gmail.com>
2024-03-26 23:27:50 +0500

    update

--- components/navText/NavText.js
+++ components/navText/NavText.js
@@ -2,16 +2,12 @@ import Link from "next/link";
 import style from "./navText.module.css";
 const NavText = () => {
   return (
-    <div className="bg-white">
-      <div className="container max-w-[1800px]">
-        <div className="p-[20px]  ">
-          <div className="flex flex-col py-2.5">
-            <Link href="#" className="underline text-[#09757a] font-medium">
-              WordPress
-            </Link>
-            <Link href="#" className="underline text-[#09757a] font-medium">
-              Managed WordPress
-            </Link>
+    <div className={style.wrapper}>
+      <div className="container">
+        <div className={style.iteam}>
+          <div className={style.links}>
+            <Link href="#">WordPress</Link>
+            <Link href="#">Managed WordPress</Link>
           </div>
 
           <h1 className={`text-5xl leading-[56px] ${style.title}`}>
---
+++ components/post/Posts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavText from "../navText/NavText";
+import Hero from "../hero/Hero";
+import Rating from "./Rating/Rating";
+
+const Posts = () => {
+  return (
+    <div>
+      <NavText />
+      <Hero />
+      <Rating />
+    </div>
+  );
+};
+
+export default Posts;
