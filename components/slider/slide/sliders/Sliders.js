6e2679b4a2104ef490686e1a049583cf2544699d
HassanThePassionate <maqsoodia777@gmail.com>
2024-03-29 02:03:21 +0500

    update

--- components/slider/slide/sliders/Sliders.js
+++ components/slider/slide/sliders/Sliders.js
@@ -7,6 +7,7 @@ import { Navigation } from "swiper/modules";
 import Title from "../title/Title";
 import Tags from "../tags/Tags";
 import Rank from "../rank/Rank";
+import Image from "next/image";
 
 const Sliders = ({ setSwiperInstance }) => {
   return (
@@ -25,44 +26,104 @@ const Sliders = ({ setSwiperInstance }) => {
       >
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={style.slide}>
-            <Title />
-            <Tags />
-            <Rank />
+            <div className={style.img}>
+              <Image
+                src="https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-1.7-Free-Download.jpg&w=1080&q=75"
+                alt="img"
+                height={500}
+                width={700}
+              />
+            </div>
+            <div className={style.side}>
+              <Title />
+              <Tags />
+              <Rank />
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "static.filehorse.com",
+        hostname: "filecr.com",
       },
     ],
   },
